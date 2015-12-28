c33e71217dbf60640b2fa99cbc3438220b4f657d
angelsystem <angelsystem1387@gmail.com>
2015-12-28 02:09:10 -0500

    perfil segue

---
+++ check/ProfileViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ProfileViewController.swift
+//  check
+//
+//  Created by angelito on 12/28/15.
+//  Copyright © 2015 walkant. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var menuButton:UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if revealViewController() != nil {
+            menuButton.target = revealViewController()
+            menuButton.action = "revealToggle:"
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
